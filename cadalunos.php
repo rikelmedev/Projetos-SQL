4b73b6052526aff7be6ba3d7f4a731541ed3b0b6
Rikelme <rikelmemini100@gmail.com>
2026-05-30 17:15:48 -0300

    Correção do cadalunos.php

--- cadalunos.php
+++ cadalunos.php
@@ -1,32 +1,51 @@
 ﻿<html>
 <head>
-<title></title>
+     <title>Cadastro de Alunos</title>
 </head>
 <body>
+
+
 <?php
 include_once ('conexao.php');
-//recupernado
+
+//recuperando
 $matricula = $_POST['matricula'];
-$nome = $_POST['nome'];
-$endereco = $_POST['endereco'];
-$cidade = $_POST['cidade'];
-$curso = $_POST['codcurso'];
+$nome      = $_POST['nome'];
+$endereco  = $_POST['endereco'];
+$cidade    = $_POST['cidade'];
+$curso     = $_POST['codcurso'];
+
+if (empty($matricula) || empty($nome)){
+
+    echo "<p>Preencha todos os campos obrigatórios (Matricula e Nome). </p>"
+     }else{
+
 //criando linha de insert
-$sqlinsert = "insert into aluno(matricula, nome, endereco, cidade, codcurso)
-VALUES ('$matricula', '$nome', '$endereco', '$cidade', '$curso')";
-//executando instrucao sql
+    $sqlinsert = "insert into aluno(matricula, nome, endereco, cidade, codcurso)
+       VALUES ('$matricula', '$nome', '$endereco', '$cidade', '$curso')";
+
+//executando instrução SQL
 $resultado = @mysqli_query($conexao, $sqlinsert);
-if (!$resultado)
-{
-die('Query Inválida: '. @mysqli_error($conexao));
-}else
-{ 
-echo "registro realizado com sucesso!";
+
+  if (!$resultado){
+   
+  echo "<p>Matricula ja cadastrada. Por favor, use matricula diferente.</p>";
+   
+      }else{
+  
+  echo "<p>Aluno cadastrado com sucesso!</p>";
 }
-mysqli_close($conexao);
+
+
+   mysqli_close($conexao);
+}
+
 ?>
+
+
 <br>
-<br>
-<input type='button' onclick="window.location='index.php';" VALUE ="Voltar">
+<!-- Botão para voltar ao menu principal -->
+<input type='button' onclick="window.location='index.php';" VALUE ="Voltar ao Menu">
+
 </body>
 </html>
