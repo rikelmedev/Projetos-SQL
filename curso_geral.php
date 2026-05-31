8b438ecd8ceeae0d9b1965ca8bac2dbcc00922f7
Rikelme <rikelmemini100@gmail.com>
2026-05-31 06:38:50 -0300

    Correção do curso_geral.php

--- curso_geral.php
+++ curso_geral.php
@@ -1,39 +1,65 @@
 <html>
-<title> Semana 01 - Exemplo 04 </title>
+   <title> Pesquisa de Cursos </title>
 <body>
+
 <center>
-<h3>Exemplo 04 - Listagem por Inicial de Nomes - Disciplina</h3>
+<h3>Pesquisa de Cursos por Nome</h3>
 </center>
+
+
 <?php
 	include_once('conexao.php');
 	$nome = $_POST['inicial'];	
-// ajustando a instrução select para ordenar por produto
+
+// EXECUÇÃO DA CONSULTA SQL
 $query = mysqli_query($conexao,"select * from curso where nome like '%$nome%'   order by nome");
-	if (!$query)
-	{
-		die('Query Inválida: ' . @mysqli_error($conexao));  
-	}
-		echo"<center>";
-	echo "<table border='1px'>";
-	echo "<tr><th width='30px' align='center'>nome </th><th width='100px'>codcurso</th>
-	<th width='250px'>coddisciplina01</th>
-	<th width='100px'>coddisciplina02</th><th width='100px'>coddisciplina03</th>
-	<tr>";
-	while($dados=mysqli_fetch_array($query)) 
-	{
 	
-		echo "<tr>";
-		echo "<td>". $dados['nome']."</td>";
-		echo "<td>". $dados['codcurso']."</td>";
-		echo "<td>". $dados['coddisciplina01']."</td>";
-		echo "<td>". $dados['coddisciplina02']."</td>";
-		echo "<td>". $dados['coddisciplina03']."</td>";		
-		echo "</tr>";
-		
-		}
+if (!$query)
+	{
+		die('Erro ao realizar a pesquisa. Tente novamente.');
+	}
+
+//VERIFICAÇÃO E EXIBIÇÃO DOS RESULTADOS
+if (mysqli_num_rows($query) == 0) {
+	echo"<center><p>Nenhum curso encontrado com o nome
+	     <strong>$nome</strong>.</p></center>";
+
+	} else {
+
+	echo"<center>";
+	echo "<table border='1'>";
+	echo "<tr>
+	      <th width='200px'>Nome</th>
+	      <th width='100px'>Cod.Curso</th>
+	      <th width='100px'>Disciplina01</th>
+	      <th width='100px'>Disciplina02</th>
+	      <th width='100px'>Disciplina03</th>
+	      <tr>";
+
+
+	while($dados=mysqli_fetch_array($query)) {
+	   echo "<tr>";
+	   echo "<td>". $dados['nome']           ."</td>";
+	   echo "<td>". $dados['codcurso']       ."</td>";
+	   echo "<td>". $dados['coddisciplina01']."</td>";
+	   echo "<td>". $dados['coddisciplina02']."</td>";
+	   echo "<td>". $dados['coddisciplina03']."</td>";		
+	   echo "</tr>";
+	}
+	
 	echo "</table>";
 	echo "</center>";
-	mysqli_close($conexao);
+
+}
+	
+mysqli_close($conexao);
+
 ?>
+
+<br>
+<center>
+	<input type="button" onclick="window.location='index.php';" value="Voltar ao Menu">
+</center>
+
 </body>
 </html>
