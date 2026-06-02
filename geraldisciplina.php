b6bbe53d2c4648f73ecf5accd0bedaf5703ca526
Rikelme <rikelmemini100@gmail.com>
2026-06-02 14:58:57 -0300

    Correção do geraldisciplina.php

--- geraldisciplina.php
+++ geraldisciplina.php
@@ -1,33 +1,54 @@
 ﻿<html>
-<title> Semana 01 - Exemplo 04 </title>
+   <title> Listagem Geral de Disciplina </title>
 <body>
 <center>
-<h3>Exemplo 04 - Listagem Geral de Nomes - Disciplina</h3>
+<h3>Listagem Geral de Disciplina</h3>
 </center>
+
 <?php
 	include_once('conexao.php');
-// ajustando a instrução select para ordenar por produto
+
+// AJUSTANDO A INSTRUÇÃO DE CONSULTA SQL
 $query = mysqli_query($conexao,"select * from disciplina order by nome_disciplina");
-	if (!$query)
-	{
-		die('Query Inválida: ' . @mysqli_error($conexao));  
-	}
-		echo"<center>";
-	echo "<table border='1px'>";
-	echo "<tr><th width='100px'>Código</th><th width='100px'>nome_disciplina</th><tr>";
-	while($dados=mysqli_fetch_array($query)) 
-	{
 	
+if (!$query)
+	{
+		die('	Erro ao consulta disciplinas. Tente Novamente.');  
+	}
+
+	//VERIFICAÇÃO E EXIBIÇÃO DE DADOS
+	if (mysqli_num_rows($query) == 0) {
+	    
+	  echo"<center><p>Nenhuma dicisplina cadastrada.</p></center>";
+
+	} else {	
+
+	  echo"<center>";
+	  echo "<table border='1px'>";
+	  echo "<tr>
+	        <th width='100px'>Código</th>
+			<th width='200px'>Nome Da Disciplina</th>
+			</tr>";
+
+	while($dados=mysqli_fetch_array($query)) {
 		echo "<tr>";
-		echo "<td>". $dados['coddisciplina']."</td>";
-		echo "<td>". $dados['nome_disciplina']."</td>";
-				
+		echo "<td>". $dados['coddisciplina']   ."</td>";
+		echo "<td>". $dados['nome_disciplina'] ."</td>";
 		echo "</tr>";
-		
-		}
+	}
+
 	echo "</table>";
 	echo "</center>";
-	mysqli_close($conexao);
+
+}	
+
+mysqli_close($conexao);
 ?>
+
+<br>
+<center>
+	<input type="button" onclick="window.location='index.php';" value="Voltar ao Menu">
+</center>
+
 </body>
 </html>
