cf3c6ddac0862e24a623369a9c58e1b5fd79b410
Rikelme <rikelmemini100@gmail.com>
2026-06-03 13:46:58 -0300

    Atualização do cadalunos.php

--- cadalunos.php
+++ cadalunos.php
@@ -15,37 +15,28 @@ $endereco  = $_POST['endereco'];
 $cidade    = $_POST['cidade'];
 $curso     = $_POST['codcurso'];
 
-if (empty($matricula) || empty($nome)){
-
-    echo "<p>Preencha todos os campos obrigatórios (Matricula e Nome). </p>"
-     }else{
-
 //criando linha de insert
     $sqlinsert = "insert into aluno(matricula, nome, endereco, cidade, codcurso)
        VALUES ('$matricula', '$nome', '$endereco', '$cidade', '$curso')";
 
 //executando instrução SQL
-$resultado = mysqli_query($conexao, $sqlinsert);
+$resultado = mysqli_query($conexao, $sql);
 
-  if (!$resultado){
-   
-  echo "<p>Matricula ja cadastrada. Por favor, use matricula diferente.</p>";
-   
-      }else{
-  
-  echo "<p>Aluno cadastrado com sucesso!</p>";
+  if (!$resultado) {
+  if (mysqli_errno($conexao) == 1062) {
+    echo "<p>Matrícula $matricula já cadastrada. Use outra.</p>";
+  } else {
+    echo "<p>Erro ao cadastrar. Tente novamente.</p>";
+  }
+} else {
+  echo "<p>Aluno $nome cadastrado com sucesso!</p>";
 }
 
-
-   mysqli_close($conexao);
-}
-
+mysqli_close($conexao);
 ?>
-
-
+ 
 <br>
-<!-- Botão para voltar ao menu principal -->
-<input type='button' onclick="window.location='index.php';" VALUE ="Voltar ao Menu">
+<input type="button" onclick="window.location='index.php';" value="Voltar ao Menu">
 
 </body>
 </html>
